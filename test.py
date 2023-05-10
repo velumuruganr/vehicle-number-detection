add9259209108f22c390edd600a37ea673ecae5b
Velmurugan R <velmuruganr165@gmail.com>
2023-05-11 02:36:13 +0530

    new

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-import app
+import detect_vehicles
 
 # video_path = "../data/videoplayback.webm"
 # video_path = "../data/sample_2.webm"
 video_path = "../data/sample_3.mp4"
-app.detect_vehicles(video_path)
+detect_vehicles.detect_vehicles(video_path)
