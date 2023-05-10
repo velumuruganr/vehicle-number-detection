f59b5d73bf075fc48245e19592cfe6066f705c96
Velmurugan R <velmuruganr165@gmail.com>
2023-05-11 02:26:44 +0530

    New

--- Readme.md
+++ Readme.md
@@ -3,6 +3,7 @@ This system is build using python with YOLOv8 - a state-of-the-art object detect
 
 ### Description:
 The system gets a video as input and detects the license number of the vehicles that are passed through in the video. The detected numbers are stored in a sqlite database which can be retrieved later for finding a vehicle in the video with its number.
+
 ### System Architecture:
 The system works based on the following architecture:
     
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sqlite3
 from time import sleep
 import cv2
 from ultralytics import YOLO
@@ -10,14 +11,18 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tess
 config = r'--oem 3 --psm 6'
 
 
+
+
 def detect_vehicles(video_path):
     # Loading the YOLOv8 model
     model = YOLO('models/yolov8n.pt')
     
+    #Capturing the video by frames
     cap = cv2.VideoCapture(video_path)
     
     classes = [2,3,5,7]
     frame_count = 1
+    
     # Looping through the video frames
     while cap.isOpened():
         success, frame = cap.read()
@@ -29,9 +34,11 @@ def detect_vehicles(video_path):
         # Running YOLOv8 inference on the frame to find the vehicles
         results = model(frame)
         
-        # annotated_frame = results[0].plot()
+        #Getting the region of vehicles from the results
         
         boxes = results[0].boxes
+        
+        # annotated_frame = results[0].plot()
         
         # Looping through the bounding boxes to process each vehicle
         for i in range(len(boxes)):
@@ -40,40 +47,41 @@ def detect_vehicles(video_path):
                     x1, y1, x2, y2 = map(int, xyxy)
                     
                     # Finding the region of a vehicle in the frame
-                    # vehicle_region = frame[y1:y2, x1:x2]
+                    vehicle_region = frame[y1:y2, x1:x2]
                     
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 1)
+                    # Number plate localization from the vehicle region using Edge Detection
+                    gray = cv2.cvtColor(vehicle_region, cv2.COLOR_BGR2GRAY)
+                    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+                    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+                    edges = cv2.Canny(thresh, 50, 150, apertureSize=3)
+                    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+                    thresh = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
                     
-                    # # Number plate localization from the vehicle region using Edge Detection
-                    # gray = cv2.cvtColor(vehicle_region, cv2.COLOR_BGR2GRAY)
-                    # gray = cv2.GaussianBlur(gray, (5, 5), 0)
-                    # edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-                    # thresh = cv2.threshold(edges, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-                    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-                    # thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+                    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+                    for contour in contours:
+                        # Compute the area of the contour
+                        x, y, w, h = cv2.boundingRect(contour)
+
+                        # Filter based on aspect ratio and area
+                        aspect_ratio = w/float(h)
+                        if aspect_ratio > 1.5 and aspect_ratio < 4 and w > 100 and h>30 :
+                            plate = vehicle_region[y:y+h, x:x+w]
+                            # cv2.rectangle(frame, (x+x1, y+y1), (x1+x+w, y1+y+h), (0, 0, 255), 1)
+                            # cv2.imwrite(f'temp/{frame_count}_plate_{i}.jpg', plate)
                     
-                    # contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                    # for contour in contours:
-                    #     # Compute the area of the contour
-                    #     # area = cv2.contourArea(contour)
-                    #     x, y, w, h = cv2.boundingRect(contour)
-                    #     # Ignore small contours
-                    #     aspect_ratio = w / float(h)
-                    #     if aspect_ratio > 1.5 and aspect_ratio < 4 and w > 100 and h > 20:
-                    #         #contour = max(contours, key=cv2.contourArea)
-                    #         plate = vehicle_region[y:y+h, x:x+w]
-                    #         cv2.imwrite(f'temp/{frame_count}_plate_{i}.jpg', plate)
-                    
-                    
-                            # # Detecting the License number in the License Plate using Tesseract OCR
-                            # gray = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
-                            # blur = cv2.GaussianBlur(gray, (3, 3), 0)
-                            # thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-                            # number = pytesseract.image_to_string(thresh,lang='eng', config=config)
+                            # Detecting the License number in the License Plate using Tesseract OCR
+                            # gray2 = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
+                            # blur2 = cv2.GaussianBlur(gray2, (3, 3), 0)
+                            # thresh2 = cv2.threshold(blur2, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+                            # 
+                            number = pytesseract.image_to_string(plate,lang='eng', config=config).replace(" ","").replace("\n","")
                             
-                            # print(number)
-                            
-                            # cv2.rectangle(frame, (x+x1, y+y1), (x1+x+w, y1+y+h), (0, 0, 255), 2)
+                            if len(number)>6:
+                                print(number)
+                                cv2.rectangle(frame, (x+x1, y+y1), (x1+x+w, y1+y+h), (0, 0, 255), 2)
+                                break
+
         cv2.imshow('Frame', frame)
         if cv2.waitKey(1) == ord('q'):
             break
@@ -89,6 +97,43 @@ def detect_vehicles(video_path):
 app = Flask(__name__)
 app.secret_key = b'Q!W@E#R$T%Y^U&I*O(P)'
 
+
+@app.route('/license_plate_list')
+def license_plate_list():
+    # Connect to the database
+    conn = sqlite3.connect('license_plates.db')
+    c = conn.cursor()
+
+    # Retrieve the license plate numbers from the database
+    c.execute("SELECT * FROM license_plates")
+    rows = c.fetchall()
+
+    # Close the database connection
+    conn.close()
+
+    # Render the license plate list template with the license plate data
+    return render_template('license_plate_list.html', license_plates=rows)
+
+
+# Define a route to search for a specific license plate number
+@app.route('/license_plate_list', methods=['POST'])
+def license_plate_search():
+    # Get the search query from the form data
+    search_query = request.form['search_query']
+
+    # Connect to the database
+    conn = sqlite3.connect('license_plates.db')
+    c = conn.cursor()
+
+    # Retrieve the license plate numbers that match the search query
+    c.execute("SELECT * FROM license_plates WHERE plate_number LIKE ?", ('%' + search_query + '%',))
+    rows = c.fetchall()
+
+    # Close the database connection
+    conn.close()
+
+    # Render the search results template with the license plate data and search query
+    return render_template('license_plate_list.html', license_plates=rows, search_query=search_query)
 
 @app.route('/')
 def index():
@@ -114,10 +159,6 @@ def detect():
     return redirect(url_for('detect_page'))
     
 
-@app.route('/result')
-def result():
-    # Rendering the result page
-    return render_template('result.html')
     
     
 if __name__ == "__main__":
